37fbfa2db876b6cd81253f79079a7ceeb5360156
xn8spicer <xn8spicer@gmail.com>
2012-11-27 14:57:39 -0500

    tuesday still need plot

--- QDAtm/ExtractMerge.R
+++ QDAtm/ExtractMerge.R
@@ -3,6 +3,7 @@
 # openProject("~/Documents/RStuff/QDAtm/QDAtm/QDAtm/q64.rqda", updateGUI=TRUE)
 
 # Select fields
+fromSource<-RQDAQuery("select  * from source inner join coding where source.id = coding.fid and coding.status = 1")
 
 # Works
 # myCodes<-RQDAQuery("select source.name as filename, source.id as fid, coding.cid 
@@ -16,12 +17,19 @@ myCodes<-RQDAQuery("select source.name as filename, source.id as fid, C.cid as c
            where freecode.id = coding.cid and coding.status = 1) C
   where source.id = C.fid and source.status = 1")
 
+myPersonLevelFile<-reshape (myCodes, v.names="code", timevar="cid", idvar="fid", direction="wide") 
+
 myCodeCats<-RQDAQuery("select  treecode.catid, codecat.name as codecat, treecode.cid
     from treecode left join codecat 
     where treecode.catid = codecat.catid
     order by treecode.catid, treecode.cid")
 
-myPersonLevelFile<-reshape (myCodes, v.names="code", timevar="cid", idvar="fid", direction="wide") 
+# if entered directly in, you can export 
+myData<-RQDAQuery("select name, file from source order by name")
+spt1<-split(myData, myData$name)
+
+lapply(names(spt1), function(x){write.table(spt1[[x]], file = paste("output", x,".txt", sep = ""))})
+
 
 # BOGUS
 # Note that you are NOT using the cid as the factor values 
